9f5db55e0941da4a05174bdbdbb740c50480665a
piupuer <piupuer@users.noreply.github.com>
2020-06-03 14:33:52 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.4b6df5e9c2cf9d02dac9a8d3ca5591e9.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2e0e53b7162140b0e9",
+    "url": "/css/401.62b730be.css"
+  },
+  {
+    "revision": "56ef1c2a451adee8f4d9",
+    "url": "/css/404.00c96426.css"
+  },
+  {
+    "revision": "c2e91c8a193a347bebae",
+    "url": "/css/app.abd9f904.css"
+  },
+  {
+    "revision": "9388f0e11b2ad9079c6a",
+    "url": "/css/chunk-2f75ae0b.1bfbe157.css"
+  },
+  {
+    "revision": "18d85fa138232ee32143",
+    "url": "/css/chunk-libs.ee57d822.css"
+  },
+  {
+    "revision": "002851492a260d1aa839",
+    "url": "/css/login.3e7393cd.css"
+  },
+  {
+    "revision": "46dce450e340c610b18c",
+    "url": "/css/profile.be8e7b57.css"
+  },
+  {
+    "revision": "27c72091ab590fb5d1c3ef90f988ddce",
+    "url": "/fonts/element-icons.27c72091.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9b70ee41d12a1cf127400d23534f7efc",
+    "url": "/fonts/element-icons.9b70ee41.woff"
+  },
+  {
+    "revision": "089007e721e1f22809c0313b670a36f1",
+    "url": "/img/401.089007e7.gif"
+  },
+  {
+    "revision": "0f4bc32b0f52f7cfb7d19305a6517724",
+    "url": "/img/404-cloud.0f4bc32b.png"
+  },
+  {
+    "revision": "a57b6f31fa77c50f14d756711dea4158",
+    "url": "/img/404.a57b6f31.png"
+  },
+  {
+    "revision": "4bd75f09955aad17c152b5293dd13c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2e0e53b7162140b0e9",
+    "url": "/js/401.b9a17ca4.js"
+  },
+  {
+    "revision": "56ef1c2a451adee8f4d9",
+    "url": "/js/404.b9d0f9f8.js"
+  },
+  {
+    "revision": "c2e91c8a193a347bebae",
+    "url": "/js/app.78f36a79.js"
+  },
+  {
+    "revision": "e518537b3b69346e69fb",
+    "url": "/js/auth-redirect.7eb0a605.js"
+  },
+  {
+    "revision": "9388f0e11b2ad9079c6a",
+    "url": "/js/chunk-2f75ae0b.fb406044.js"
+  },
+  {
+    "revision": "5e2fc723c780c4f66aa7",
+    "url": "/js/chunk-78b23325.28b84d3e.js"
+  },
+  {
+    "revision": "80ed7230e0db4c57e1d1",
+    "url": "/js/chunk-elementUI.22466c51.js"
+  },
+  {
+    "revision": "18d85fa138232ee32143",
+    "url": "/js/chunk-libs.fa7c7697.js"
+  },
+  {
+    "revision": "002851492a260d1aa839",
+    "url": "/js/login.df39c704.js"
+  },
+  {
+    "revision": "46dce450e340c610b18c",
+    "url": "/js/profile.6d5045fb.js"
+  },
+  {
+    "revision": "e650c2302dbfb25bfa81",
+    "url": "/js/redirect.35971e3d.js"
+  },
+  {
+    "revision": "0736a32abeb5c001e3e8",
+    "url": "/js/runtime.204d23e3.js"
+  },
+  {
+    "revision": "5b1b3f3d78e7eca45f642ed55c1d5cf9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bd1589852f60e698f0f6345c5dfc2c64",
+    "url": "/tinymce/emojis.min.js"
+  },
+  {
+    "revision": "475937256545f9401f1b0836c40b094a",
+    "url": "/tinymce/langs/es.js"
+  },
+  {
+    "revision": "d7d9556de17ab7fb644d03963addad5c",
+    "url": "/tinymce/langs/ja.js"
+  },
+  {
+    "revision": "242662192c74744d8c9164f457d82f93",
+    "url": "/tinymce/langs/ko_KR.js"
+  },
+  {
+    "revision": "03752a33e9043da26c0b4af2444fcdbc",
+    "url": "/tinymce/langs/zh_CN.js"
+  },
+  {
+    "revision": "128d68a7502304b27acb407e546f5c4e",
+    "url": "/tinymce/skins/content.inline.min.css"
+  },
+  {
+    "revision": "3208440bc70a5a03481fa8bc6f4b8409",
+    "url": "/tinymce/skins/content.min.css"
+  },
+  {
+    "revision": "0684a64086ad1114949a1e51f06aa750",
+    "url": "/tinymce/skins/content.mobile.min.css"
+  },
+  {
+    "revision": "baecf466c40e709e7ffdbc935fc0813a",
+    "url": "/tinymce/skins/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": "2ad81bd1728be5d410312a4b7d80fdc2",
+    "url": "/tinymce/skins/skin.min.css"
+  },
+  {
+    "revision": "ddda741d80c4bc322faf2331d0e1d2fc",
+    "url": "/tinymce/skins/skin.mobile.min.css"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/precache-manifest.4b6df5e9c2cf9d02dac9a8d3ca5591e9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (e.data) {
+    if (e.data === 'skipWaiting') {
+      self.skipWaiting()
+    }
+  }
+})
+
+/* eslint-disable no-undef */
+workbox.core.clientsClaim()
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
